33f90a701621f659c1b83af8ea13a2efa16adecc
Matt Soucy <git@msoucy.me>
2020-01-18 16:09:20 -0500

    Use Java manifest for Git data

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -7,17 +7,26 @@
 
 package frc.robot;
 
+import java.io.IOException;
+import java.net.URL;
+import java.util.jar.Attributes;
+import java.util.jar.Manifest;
+
 import com.chopshop166.chopshoplib.RobotUtils;
 import com.chopshop166.chopshoplib.controls.ButtonXboxController;
 import com.chopshop166.chopshoplib.controls.ButtonXboxController.XBoxButton;
+import com.google.common.io.Resources;
 
 import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.GenericHID.Hand;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.XboxController;
+import edu.wpi.first.wpilibj.shuffleboard.Shuffleboard;
+import edu.wpi.first.wpilibj.shuffleboard.ShuffleboardTab;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.button.JoystickButton;
 import frc.robot.maps.RobotMap;
 import frc.robot.maps.TempestMap;
@@ -36,108 +45,127 @@ import frc.robot.subsystems.Shooter;
  */
 public class Robot extends TimedRobot {
 
-  private Command autonomousCommand;
-  private ButtonXboxController driveController = new ButtonXboxController(1);
-  private JoystickButton aButton = new JoystickButton(driveController, XBoxButton.A.get());
-  private JoystickButton bButton = new JoystickButton(driveController, XBoxButton.B.get());
-  private JoystickButton xButton = new JoystickButton(driveController, XBoxButton.X.get());
-  private JoystickButton yButton = new JoystickButton(driveController, XBoxButton.Y.get());
+    private Command autonomousCommand;
+    private ButtonXboxController driveController = new ButtonXboxController(1);
+    private JoystickButton aButton = new JoystickButton(driveController, XBoxButton.A.get());
+    private JoystickButton bButton = new JoystickButton(driveController, XBoxButton.B.get());
+    private JoystickButton xButton = new JoystickButton(driveController, XBoxButton.X.get());
+    private JoystickButton yButton = new JoystickButton(driveController, XBoxButton.Y.get());
 
-  RobotMap map = new TempestMap();
+    RobotMap map = new TempestMap();
 
-  final private Drive drive = new Drive(map.getDriveMap());
-  final private Intake intake = new Intake(map.getIntakeMap());
-  final private Shooter shooter = new Shooter(map.getShooterMap());
-  final private ControlPanel controlPanel = new ControlPanel(map.getControlPanelMap());
-  final private Lift lift = new Lift(map.getLiftMap());
+    final private Drive drive = new Drive(map.getDriveMap());
+    final private Intake intake = new Intake(map.getIntakeMap());
+    final private Shooter shooter = new Shooter(map.getShooterMap());
+    final private ControlPanel controlPanel = new ControlPanel(map.getControlPanelMap());
+    final private Lift lift = new Lift(map.getLiftMap());
 
-  final private SendableChooser<Command> autoChooser = new SendableChooser<>();
+    final private SendableChooser<Command> autoChooser = new SendableChooser<>();
+    final private static String UNKNOWN_VALUE = "???";
 
-  /**
-   * This function is run when the robot is first started up and should be used
-   * for any initialization code.
-   */
-  @Override
-  public void robotInit() {
-    configureButtonBindings();
+    /**
+     * This function is run when the robot is first started up and should be used
+     * for any initialization code.
+     */
+    @Override
+    public void robotInit() {
+        configureButtonBindings();
 
-    autoChooser.setDefaultOption("Nothing", null);
+        autoChooser.setDefaultOption("Nothing", new InstantCommand());
 
-    drive.setDefaultCommand(drive.drive(driveController::getTriggers, () -> driveController.getX(Hand.kLeft)));
+        final ShuffleboardTab tab = Shuffleboard.getTab("BuildData");
 
-  }
+        try {
+            final URL manifestURL = Resources.getResource("META-INF/MANIFEST.MF");
+            final Manifest manifest = new Manifest(manifestURL.openStream());
+            final Attributes attrs = manifest.getMainAttributes();
 
-  /**
-   * This function is called every robot packet, no matter the mode. Use this for
-   * items like diagnostics that you want ran during disabled, autonomous,
-   * teleoperated and test.
-   *
-   * <p>
-   * This runs after the mode specific periodic functions, but before LiveWindow
-   * and SmartDashboard integrated updating.
-   */
-  @Override
-  public void robotPeriodic() {
-    // Runs the Scheduler. This is responsible for polling buttons, adding
-    // newly-scheduled
-    // commands, running already-scheduled commands, removing finished or
-    // interrupted commands,
-    // and running subsystem periodic() methods. This must be called from the
-    // robot's periodic
-    // block in order for anything in the Command-based framework to work.
-    CommandScheduler.getInstance().run();
-  }
+            tab.add("Git Hash", attrs.getValue("Git-Hash")).withPosition(0, 0);
+            tab.add("Git Branch", attrs.getValue("Git-Branch")).withPosition(3, 0);
+            tab.add("Build Time", attrs.getValue("Build-Time")).withPosition(1, 0).withSize(2, 1);
+            tab.add("Git Files", attrs.getValue("Git-Files")).withPosition(0, 1).withSize(4, 1);
+        } catch (IOException ex) {
+            // Could not read the manifest, just send dummy values
+            tab.add("Git Hash", UNKNOWN_VALUE).withPosition(0, 0);
+            tab.add("Git Branch", UNKNOWN_VALUE).withPosition(0, 1);
+            tab.add("Git Files", UNKNOWN_VALUE).withPosition(1, 0);
+            tab.add("Build Time", UNKNOWN_VALUE).withPosition(1, 1);
+        }
 
-  /**
-   * This function is called once each time the robot enters Disabled mode.
-   */
-  @Override
-  public void disabledInit() {
-    RobotUtils.resetAll(this);
-  }
-
-  /**
-   * This autonomous runs the autonomous command selected by your
-   * {@link RobotContainer} class.
-   */
-  @Override
-  public void autonomousInit() {
-    autonomousCommand = autoChooser.getSelected();
-
-    // schedule the autonomous command (example)
-    if (autonomousCommand != null) {
-      autonomousCommand.schedule();
+        drive.setDefaultCommand(drive.drive(driveController::getTriggers, () -> driveController.getX(Hand.kLeft)));
     }
-  }
 
-  @Override
-  public void teleopInit() {
-    // This makes sure that the autonomous stops running when
-    // teleop starts running. If you want the autonomous to
-    // continue until interrupted by another command, remove
-    // this line or comment it out.
-    if (autonomousCommand != null) {
-      autonomousCommand.cancel();
+    /**
+     * This function is called every robot packet, no matter the mode. Use this for
+     * items like diagnostics that you want ran during disabled, autonomous,
+     * teleoperated and test.
+     *
+     * <p>
+     * This runs after the mode specific periodic functions, but before LiveWindow
+     * and SmartDashboard integrated updating.
+     */
+    @Override
+    public void robotPeriodic() {
+        // Runs the Scheduler. This is responsible for polling buttons, adding
+        // newly-scheduled
+        // commands, running already-scheduled commands, removing finished or
+        // interrupted commands,
+        // and running subsystem periodic() methods. This must be called from the
+        // robot's periodic
+        // block in order for anything in the Command-based framework to work.
+        CommandScheduler.getInstance().run();
     }
-  }
 
-  @Override
-  public void testInit() {
-    // Cancels all running commands at the start of test mode.
-    CommandScheduler.getInstance().cancelAll();
-  }
+    /**
+     * This function is called once each time the robot enters Disabled mode.
+     */
+    @Override
+    public void disabledInit() {
+        RobotUtils.resetAll(this);
+    }
 
-  /**
-   * Use this method to define your button->command mappings. Buttons can be
-   * created by instantiating a {@link GenericHID} or one of its subclasses
-   * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
-   * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
-   */
-  private void configureButtonBindings() {
-    aButton.whenHeld(intake.intake());
-    bButton.whenHeld(intake.discharge());
-    xButton.whenHeld(controlPanel.spinForwards());
-    yButton.whenHeld(controlPanel.spinBackwards());
+    /**
+     * This autonomous runs the autonomous command selected by your
+     * {@link RobotContainer} class.
+     */
+    @Override
+    public void autonomousInit() {
+        autonomousCommand = autoChooser.getSelected();
 
-  }
+        // schedule the autonomous command (example)
+        if (autonomousCommand != null) {
+            autonomousCommand.schedule();
+        }
+    }
+
+    @Override
+    public void teleopInit() {
+        // This makes sure that the autonomous stops running when
+        // teleop starts running. If you want the autonomous to
+        // continue until interrupted by another command, remove
+        // this line or comment it out.
+        if (autonomousCommand != null) {
+            autonomousCommand.cancel();
+        }
+    }
+
+    @Override
+    public void testInit() {
+        // Cancels all running commands at the start of test mode.
+        CommandScheduler.getInstance().cancelAll();
+    }
+
+    /**
+     * Use this method to define your button->command mappings. Buttons can be
+     * created by instantiating a {@link GenericHID} or one of its subclasses
+     * ({@link edu.wpi.first.wpilibj.Joystick} or {@link XboxController}), and then
+     * passing it to a {@link edu.wpi.first.wpilibj2.command.button.JoystickButton}.
+     */
+    private void configureButtonBindings() {
+        aButton.whenHeld(intake.intake());
+        bButton.whenHeld(intake.discharge());
+        xButton.whenHeld(controlPanel.spinForwards());
+        yButton.whenHeld(controlPanel.spinBackwards());
+
+    }
 }
