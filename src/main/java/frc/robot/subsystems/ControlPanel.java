0613f457b55f57aa31958f10ecf2770af72e5a0a
Rohan Sreenivasan <rohan_s885@yahoo.com>
2020-01-18 20:36:08 -0500

    Color wheel stage 2 rotation spin
    This code will spin the color wheel to roughly 4 rotations

--- src/main/java/frc/robot/subsystems/ControlPanel.java
+++ src/main/java/frc/robot/subsystems/ControlPanel.java
@@ -2,7 +2,9 @@ package frc.robot.subsystems;
 
 import com.chopshop166.chopshoplib.outputs.SendableSpeedController;
 
+import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.InstantCommand;
 import edu.wpi.first.wpilibj2.command.StartEndCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.maps.RobotMap;
@@ -41,16 +43,17 @@ public class ControlPanel extends SubsystemBase {
         }, this);
     }
 
-    private final I2C.Port i2cPort = I2C.Port.kOnboard;
+    public final I2C.Port i2cPort = I2C.Port.kOnboard;
 
-    private final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
+    public final ColorSensorV3 m_colorSensor = new ColorSensorV3(i2cPort);
 
-    private final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
-    private final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
-    private final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
-    private final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
+    public final Color kBlueTarget = ColorMatch.makeColor(0.143, 0.427, 0.429);
+    public final Color kGreenTarget = ColorMatch.makeColor(0.197, 0.561, 0.240);
+    public final Color kRedTarget = ColorMatch.makeColor(0.561, 0.232, 0.114);
+    public final Color kYellowTarget = ColorMatch.makeColor(0.361, 0.524, 0.113);
 
-    private final ColorMatch m_colorMatcher = new ColorMatch();
+    public final ColorMatch m_colorMatcher = new ColorMatch();
+    public static String colorString1 = "";
 
     public void detectColor() {
 
@@ -63,6 +66,7 @@ public class ControlPanel extends SubsystemBase {
 
         String colorString;
         ColorMatchResult match = m_colorMatcher.matchClosestColor(detectedColor);
+        colorString1 = "";
 
         if (match.color == kBlueTarget) {
             colorString = "Blue";
@@ -84,7 +88,50 @@ public class ControlPanel extends SubsystemBase {
         SmartDashboard.putNumber("Blue", detectedColor.blue);
         SmartDashboard.putNumber("Confidence", match.confidence);
         SmartDashboard.putString("Detected Color", colorString);
+        colorString1 = colorString;
 
     }
 
-}
+    public void stageTwoRotation() {
+
+        int iRed = 0;
+        int iBlue = 0;
+        int iGreen = 0;
+        int iYellow = 0;
+        int iUnk = 0;
+        spinForwards();
+
+        for (int i = 0; i < 100; i++) {
+            detectColor();
+            if (colorString1 == "Red") {
+                iRed++;
+
+            } else if (colorString1 == "Blue") {
+                iBlue++;
+            } else if (colorString1 == "Green") {
+                iGreen++;
+            } else if (colorString1 == "Yellow") {
+                iYellow++;
+            } else if (colorString1 == "Blue") {
+                iBlue++;
+            } else if (colorString1 == "Unknown") {
+                iUnk++;
+            }
+
+            if (iGreen + iRed + iUnk + iYellow + iBlue == 36) {
+                // this is 36 because that means that the wheel would have spun 4 rotations
+
+            }
+
+        }
+
+    }
+
+};
+
+// I want to make a function that when engaged will take a reading of the color,
+// mark that in an array, then, slowly rotate the color wheel and record every
+// new color as a
+// new member of the array, once the array has reached 24-40 changes (8 changes
+// per rotation, 3-5 rotations). once this number of members has been reached in
+// the array the motors stop
