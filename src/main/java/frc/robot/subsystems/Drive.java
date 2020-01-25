49c9bb43b35cf82226ed754a82b263133f3dfeb8
Jeffrey Burke <jburke0519@gmail.com>
2020-01-25 13:52:06 -0500

    Documentation

--- src/main/java/frc/robot/subsystems/Drive.java
+++ src/main/java/frc/robot/subsystems/Drive.java
@@ -10,12 +10,35 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.maps.RobotMap;
 
+/**
+ * 1) What does it do? Makes motors turn a certain amount depending on how much
+ * an axis is triggered
+ * 
+ * 2) What modes does it have? Has a mode to differentiate between right and
+ * left motor groups.
+ * 
+ * 3) What interactions does it have with other subsystems? Does not have any
+ * interactions with other systems other than the space it shares on the robot
+ * 
+ * 4) How is it triggered -> OI? Triggered by motors to make the robots wheels
+ * spin a certain direction.
+ * 
+ * 5) Does it store any state? Right and Left motor groups.
+ * 
+ * 6) Sensors? Speed Controllers
+ */
 public class Drive extends SubsystemBase {
 
     private final SendableSpeedController rightMotorGroup;
     private final SendableSpeedController leftMotorGroup;
     private final DifferentialDrive driveTrain;
 
+    /**
+     * Defines the left and right motor(s) and then puts them into a differential
+     * drive
+     * 
+     * @param map represents the drive map
+     */
     public Drive(RobotMap.DriveMap map) {
         super();
         rightMotorGroup = map.right();
@@ -23,6 +46,14 @@ public class Drive extends SubsystemBase {
         driveTrain = new DifferentialDrive(leftMotorGroup, rightMotorGroup);
     }
 
+    /**
+     * Maps the drive axises
+     * 
+     * @param forward defines the forward direction
+     * @param turn    defines the direction to turn
+     * @return returns a run command so drive will stay running as long as drive is
+     *         being called
+     */
     public CommandBase drive(DoubleSupplier forward, DoubleSupplier turn) {
         return new RunCommand(() -> {
             double yAxis = forward.getAsDouble();
