ad34758b75ea87c525c38d83074bb0830d523042
Austin Ford <redpanda64@protonmail.com>
2020-01-25 15:16:35 -0500

    Added Questions - Shooter

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -5,6 +5,20 @@ import com.chopshop166.chopshoplib.outputs.SendableSpeedController;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.maps.RobotMap;
 
+/**
+ * What does it do?
+ * 
+ * What modes does it have?
+ * 
+ * what interactions does it have with other subsystems?
+ * 
+ * How is it triggered -> OI?
+ * 
+ * Does it store any state?
+ * 
+ * Sensors?
+ * 
+ */
 public class Shooter extends SubsystemBase {
 
     private SendableSpeedController shooterWheelMotor;
