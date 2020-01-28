87db78b45c8c9691cd301aa13fcf916884bd9675
Austin Ford <redpanda64@protonmail.com>
2020-01-27 19:41:57 -0500

    Answered the Questions
    In Grave Detail

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -6,19 +6,19 @@ import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.maps.RobotMap;
 
 /**
- * What does it do?
+ * What does it do? Starts spinning the belt after the button is toggled on.
  * 
- * What modes does it have?
+ * What modes does it have? On and off.
  * 
- * what interactions does it have with other subsystems?
+ * what interactions does it have with other subsystems? Tells the indexer if the shooter is up to speed.
  * 
- * How is it triggered -> OI?
+ * How is it triggered/OI? A Toggle Conveyer button (y).
  * 
- * Does it store any state?
+ * Does it store any state? Spinning or not spinning.
  * 
- * Sensors?
- * 
+ * Sensors? No.
  */
+
 public class Shooter extends SubsystemBase {
 
     private SendableSpeedController shooterWheelMotor;
@@ -28,4 +28,5 @@ public class Shooter extends SubsystemBase {
         shooterWheelMotor = map.shooterWheel();
     }
 
+    public 
 }
