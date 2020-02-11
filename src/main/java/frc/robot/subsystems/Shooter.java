da3b58fa9d355412fbe47e5e64af557d41964b4f
Austin Ford <redpanda64@protonmail.com>
2020-02-11 18:35:16 -0500

    Changed variable names
    Also made sure it accounted for the height of the shooter.
    
    Please work.

--- src/main/java/frc/robot/subsystems/Shooter.java
+++ src/main/java/frc/robot/subsystems/Shooter.java
@@ -1,6 +1,5 @@
 package frc.robot.subsystems;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import com.chopshop166.chopshoplib.outputs.SendableSpeedController;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
@@ -20,23 +19,22 @@ import java.lang.Math;
 public class Shooter extends SubsystemBase {
 
     private final SendableSpeedController shooterWheelMotor;
-    public static double speed;
-    public static double gravity;
-    public static double theta;
-    public static double y;
+    public final double shooterHeight;
+    public static double verticalDistance;
+    public final static double GRAVITY = 386.09;
+    public final static double THETA = 37;
+    public final static double TARGET_HEIGHT = 98.25;
 
     public Shooter(final RobotMap.ShooterMap map) {
         super();
-        gravity = 386.09;
-        theta = 37;
-        y = 98.25;
-        speed = SmartDashboard.getNumber("YeetSpeed", speed);
+        shooterHeight = map.shooterHeight();
         shooterWheelMotor = map.shooterWheel();
+        verticalDistance = TARGET_HEIGHT - shooterHeight;
     }
 
     public CommandBase spinUp() {
         return new InstantCommand(() -> {
-            shooterWheelMotor.set(speed);
+            shooterWheelMotor.set(8.5); // Make a calculateRPM() function or something to get the speed
         }, this);
     }
 
@@ -48,21 +46,21 @@ public class Shooter extends SubsystemBase {
 
     /**
      * Finds the needed velocity to hit target (inches) (x,y) with a given launch
-     * angle (theta).
+     * angle (THETA).
      */
 
-    public static double calculateVelocity(double x) {
+    public static double calculateVelocity(final double distanceToTarget) {
         /**
          * xtanθ has to be greater than y- if it isn't then it is impossible to reach
          * that point with given launch angle. Eg. trying to hit a target at (1, 100)
          * with a launch angle of 1 degree.
          */
-        if (x * Math.tan(Math.toRadians(theta)) >= y) {
-            double gravitySide = gravity * x * x;
-            double tanSide = x * Math.tan(Math.toRadians(theta)) - y;
-            double cosSide = Math.cos(Math.toRadians(theta)) * Math.cos(Math.toRadians(theta));
+        if (distanceToTarget * Math.tan(Math.toRadians(THETA)) >= verticalDistance) {
+            final double GRAVITY_SIDE = GRAVITY * distanceToTarget * distanceToTarget;
+            final double TAN_SIDE = distanceToTarget * Math.tan(Math.toRadians(THETA)) - verticalDistance;
+            final double COS_SIDE = Math.cos(Math.toRadians(THETA)) * Math.cos(Math.toRadians(THETA));
 
-            double velocity = Math.sqrt(gravitySide / tanSide / cosSide / 2);
+            final double velocity = Math.sqrt(GRAVITY_SIDE / TAN_SIDE / COS_SIDE / 2);
             return velocity;
         } else {
             return 0;
