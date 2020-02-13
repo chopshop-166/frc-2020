e01ed3497494558c10e412e8afbe2a7d0fb89073
David Desfosse <daviddesfosse@gmail.com>
2020-02-12 20:55:42 -0500

    Modified shooter to use a PID speed controller

--- src/main/java/frc/robot/maps/RobotMap.java
+++ src/main/java/frc/robot/maps/RobotMap.java
@@ -5,8 +5,10 @@ import com.chopshop166.chopshoplib.outputs.EncodedSpeedController;
 import com.chopshop166.chopshoplib.outputs.IDSolenoid;
 import com.chopshop166.chopshoplib.outputs.MockDSolenoid;
 import com.chopshop166.chopshoplib.outputs.MockSpeedController;
+import com.chopshop166.chopshoplib.outputs.PIDSpeedController;
 import com.chopshop166.chopshoplib.outputs.SendableSpeedController;
 import com.chopshop166.chopshoplib.sensors.MockEncoder;
+import edu.wpi.first.wpilibj.smartdashboard.SendableBuilder;
 
 public class RobotMap {
 
@@ -44,8 +46,81 @@ public class RobotMap {
     }
 
     public static class ShooterMap {
-        public SendableSpeedController shooterWheel() {
-            return new MockSpeedController();
+        public PIDSpeedController shooterWheel() {
+            return new PIDSpeedController() {
+
+                @Override
+                public void initSendable(SendableBuilder builder) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void set(double speed) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public double get() {
+                    // TODO Auto-generated method stub
+                    return 0;
+                }
+
+                @Override
+                public void setInverted(boolean isInverted) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public boolean getInverted() {
+                    // TODO Auto-generated method stub
+                    return false;
+                }
+
+                @Override
+                public void disable() {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void stopMotor() {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void pidWrite(double output) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void setP(double kp) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void setI(double ki) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void setD(double kd) {
+                    // TODO Auto-generated method stub
+
+                }
+
+                @Override
+                public void setSetpoint(double setPoint) {
+                    // TODO Auto-generated method stub
+
+                }
+            };
         }
 
         public double shooterHeight() {
