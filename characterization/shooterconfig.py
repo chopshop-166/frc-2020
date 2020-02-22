0066e3550c28a676baffc4f86137c5037c8f0525
Andrew <amlefty5@gmail.com>
2020-02-22 14:11:04 -0500

    Added basic characterization dictionary files
    Some instructions included in the files

---
+++ characterization/shooterconfig.py
@@ -0,0 +1,23 @@
+{
+    # To run, type "pip install --upgrade frc-characterization"
+    # Then, "type frc-characterization simple-motor new"
+    # Warning: This project type is for BRUSHLESS motors ONLY!
+    # Unit of analysis
+    # Options:
+    # 'Degrees'
+    # 'Radians'
+    # 'Rotations'
+    "units": "Rotations",
+    # Ports for the flywheel motors
+    # If you only have 1 motor all the below arrays should only have one element
+    # The first port is the port for the motor whose encoder will be used
+    "motorPorts": [23,26],
+    # Note: Inversions of the slaves (i.e. any motor *after* the first) are
+    # *with respect to their master*. This is different from the other poject types!
+    # Inversions for the flywheel motor(s)
+    "motorsInverted": [True, False],
+    # The total gear reduction between the motor and the wheels, expressed as
+    # a fraction [motor turns]/[wheel turns]
+    "gearing": 1,
+}
+
