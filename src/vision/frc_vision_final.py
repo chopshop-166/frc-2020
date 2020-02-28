d090114aba4625b88a2156b328de0980df502ae6
Unknown <quinlan.duval@comcast.net>
2020-02-28 01:50:28 -0500

    PR Fixes

--- src/vision/frc_vision_final.py
+++ src/vision/frc_vision_final.py
@@ -11,15 +11,18 @@ import time
 from cscore import CameraServer
 from networktables import NetworkTables
 
+debugging = False
+
 ANGLE_THRESHOLD = 40
 
 # Resolution
 WIDTH = 640
 HEIGHT = 480
 POINT_SAMPLES = 5
+
 # Angle value of each pixel
 FOV_ANGLE = 82.5
-PIXEL_ANGLE = FOV_ANGLE/WIDTH
+PIXEL_ANGLE = FOV_ANGLE / WIDTH
 
 OldStream = True
 
@@ -31,8 +34,20 @@ outputStream = cs.putVideo("Color", WIDTH, HEIGHT)
 
 # Takes in slopes x and y, tests if they are equal to each other or any previously verified line
 
+
 def swapStream(isShooting):
-    if not isShooting:
+    if isShooting:
+        # s.set_option(rs2.option.auto_exposure_mode, False)
+        s.set_option(rs2.option.brightness, 0)
+        s.set_option(rs2.option.contrast, 100)
+        s.set_option(rs2.option.exposure, 45)
+        s.set_option(rs2.option.gain, 75)
+        s.set_option(rs2.option.gamma, 100)
+        s.set_option(rs2.option.hue, 0)
+        s.set_option(rs2.option.saturation, 50)
+        s.set_option(rs2.option.sharpness, 0)
+        s.set_option(rs2.option.white_balance, 2800)
+    else:
         # s.set_option(rs2.option.auto_exposure_mode, True)
         s.set_option(rs2.option.brightness, 0)
         s.set_option(rs2.option.contrast, 50)
@@ -43,39 +58,33 @@ def swapStream(isShooting):
         s.set_option(rs2.option.saturation, 64)
         s.set_option(rs2.option.sharpness, 50)
         s.set_option(rs2.option.white_balance, 4600)
-    else:
-        # s.set_option(rs2.option.auto_exposure_mode, False)
-        s.set_option(rs2.option.brightness, 0)
-        s.set_option(rs2.option.contrast, 100)
-        s.set_option(rs2.option.exposure, 45)
-        s.set_option(rs2.option.gain, 75)
-        s.set_option(rs2.option.gamma, 100)
-        s.set_option(rs2.option.hue, 0)
-        s.set_option(rs2.option.saturation, 50)
-        s.set_option(rs2.option.sharpness, 0)
-        s.set_option(rs2.option.white_balance, 2800) 
+
 
 def unequal(new, old_list):
     variance = 5
     for i in old_list:
         x1, y1, x2, y2 = i[0]
-        old_slope = degrees(np.arctan((y2 - y1)/(x2 - x1)))
+        old_slope = degrees(np.arctan((y2 - y1) / (x2 - x1)))
         if abs(new - old_slope) < variance:
             return False
     return True
 
 
-def newLine(filtered_lines, new_line, filtered_line_img, x1, y1, x2, y2, X_TOTAL, Y_TOTAL):
+def newLine(
+    filtered_lines, new_line, filtered_line_img, x1, y1, x2, y2, X_TOTAL, Y_TOTAL
+):
     filtered_lines.append(new_line)
-    cv2.line(filtered_line_img, (x1, y1), (x2, y2), (0, 255, 0), 1)
+    if debugging:
+        cv2.line(filtered_line_img, (x1, y1), (x2, y2), (0, 255, 0), 1)
     X_TOTAL += x1 + x2
     Y_TOTAL += y1 + y2
 
     return X_TOTAL, Y_TOTAL
 
-NetworkTables.initialize(server='roborio-166-frc.local')
 
-sd = NetworkTables.getTable('SmartDashboard')
+NetworkTables.initialize(server="roborio-166-frc.local")
+
+sd = NetworkTables.getTable("SmartDashboard")
 # sd.putNumber('someNumber', 1234)
 # otherNumber = sd.getNumber('otherNumber')
 
@@ -98,6 +107,8 @@ s.set_option(rs2.option.white_balance, 2800)
 
 VALS = []
 
+FILTERED_LINE_IMG = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+
 while True:
     start_time = time.time()
 
@@ -118,112 +129,138 @@ while True:
     if isShooting:
         # Convert from RGB to HSV, helps with filtering
         HSV = cv2.cvtColor(IMG, cv2.COLOR_BGR2HSV)
-    
+
         # Define upper and lower bounds for HSV variables
         LOWER_COLOR = np.array([70, 80, 255])
         UPPER_COLOR = np.array([95, 180, 255])
-    
+
         # Create mask within hsv range
         MASK = cv2.inRange(HSV, LOWER_COLOR, UPPER_COLOR)
-    
-        # Various blur method testings (buffer for pixel imperfections)
-        # BLUR = cv2.GaussianBlur(MASK, (3, 3), 0)
+
+        # Blur image (buffer for pixel imperfections)
         MEDIAN = cv2.medianBlur(MASK, 5)
-    
-        # Edge detection on each test for use in line detection
-        # BLUR_EDGES = cv2.Canny(BLUR, 100, 200)
-        # MASK_EDGES = cv2.Canny(MASK, 100, 200)
+
+        # Edge detection for use in line detection
         MED_EDGES = cv2.Canny(MEDIAN, 50, 150)
-    
-        # Empty image for drawing lines (testing)
-        FILTERED_LINE_IMG = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
-        LINE_IMG = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
-    
+
+        if debugging:
+            BLUR = cv2.GaussianBlur(MASK, (3, 3), 0)
+
+            BLUR_EDGES = cv2.Canny(BLUR, 100, 200)
+            MASK_EDGES = cv2.Canny(MASK, 100, 200)
+
+            # Empty image for drawing lines (testing)
+            FILTERED_LINE_IMG = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+            LINE_IMG = np.zeros((HEIGHT, WIDTH, 3), np.uint8)
+
         # Find lines in selected image
-        LINES = cv2.HoughLinesP(MED_EDGES, 1, radians(.5), 25, maxLineGap=25)
-    
+        LINES = cv2.HoughLinesP(MED_EDGES, 1, radians(0.5), 25, maxLineGap=25)
+
         # If there are lines:
         if LINES is not None:
             NUM_LINES = len(LINES)
             FILTERED_LINES = []
             X_TOTAL = 0
             Y_TOTAL = 0
-    
+
             for NEW_LINE in LINES:
                 x1, y1, x2, y2 = NEW_LINE[0]
-                new_slope = degrees(np.arctan((y2 - y1)/(x2 - x1)))
-    
+                new_slope = degrees(np.arctan((y2 - y1) / (x2 - x1)))
+
                 # Checks if we have verified lines, and makes a new line based on that.
                 if FILTERED_LINES:
-                    if (new_slope < -ANGLE_THRESHOLD or new_slope > ANGLE_THRESHOLD) and unequal(new_slope, FILTERED_LINES):
+                    if (
+                        new_slope < -ANGLE_THRESHOLD or new_slope > ANGLE_THRESHOLD
+                    ) and unequal(new_slope, FILTERED_LINES):
                         X_TOTAL, Y_TOTAL = newLine(
-                            FILTERED_LINES, NEW_LINE, FILTERED_LINE_IMG, x1, y1, x2, y2, X_TOTAL, Y_TOTAL)
+                            FILTERED_LINES,
+                            NEW_LINE,
+                            FILTERED_LINE_IMG,
+                            x1,
+                            y1,
+                            x2,
+                            y2,
+                            X_TOTAL,
+                            Y_TOTAL,
+                        )
                 elif new_slope < -ANGLE_THRESHOLD or new_slope > ANGLE_THRESHOLD:
-                    X_TOTAL, Y_TOTAL = newLine(FILTERED_LINES, NEW_LINE,
-                        FILTERED_LINE_IMG, x1, y1, x2, y2, X_TOTAL, Y_TOTAL)
-    
-            NUM_LINES=len(FILTERED_LINES)
+                    X_TOTAL, Y_TOTAL = newLine(
+                        FILTERED_LINES,
+                        NEW_LINE,
+                        FILTERED_LINE_IMG,
+                        x1,
+                        y1,
+                        x2,
+                        y2,
+                        X_TOTAL,
+                        Y_TOTAL,
+                    )
+
+            NUM_LINES = len(FILTERED_LINES)
             if FILTERED_LINES:
-                X_AVG=0
-                Y_AVG=0
-    
+                X_AVG = 0
+                Y_AVG = 0
+
                 if len(VALS) == POINT_SAMPLES:
                     VALS.pop(0)
-                    VALS.append([X_TOTAL/(2*NUM_LINES), Y_TOTAL/(2*NUM_LINES)])
-    
+                    VALS.append([X_TOTAL / (2 * NUM_LINES), Y_TOTAL / (2 * NUM_LINES)])
+
                     # TODO: Test if you could keep this continuous, not re-add everything every loop
                     # Subtract Popped val, add new val?
                     for VAL in VALS:
                         X_VAL, Y_VAL = VAL
                         X_AVG += X_VAL
                         Y_AVG += Y_VAL
-    
-                    X_AVG=int(X_AVG / POINT_SAMPLES)
-                    Y_AVG=int(Y_AVG / POINT_SAMPLES)
-    
-                    offset = 2 * (X_AVG - (WIDTH/2)) / WIDTH
-                    cv2.circle(IMG, (X_AVG, Y_AVG),
-                               5, [255, 255, 255], -1)
+
+                    X_AVG = int(X_AVG / POINT_SAMPLES)
+                    Y_AVG = int(Y_AVG / POINT_SAMPLES)
+
+                    offset = 2 * (X_AVG - (WIDTH / 2)) / WIDTH
+                    cv2.circle(IMG, (X_AVG, Y_AVG), 5, [255, 255, 255], -1)
                     try:
                         dist_to_target = depth.get_distance(X_AVG, Y_AVG)
                     except:
                         None
-    
+
                     # Smart Dashboard variables
                     sd.putBoolean("Sees Target", True)
                     sd.putNumber("Ratio Offset", offset)
-                    sd.putNumber("Angle Offset", (PIXEL_ANGLE * X_AVG) - (FOV_ANGLE / 2))
+                    sd.putNumber(
+                        "Angle Offset", (PIXEL_ANGLE * X_AVG) - (FOV_ANGLE / 2)
+                    )
                     sd.putNumber("Distance To Target", dist_to_target)
-    
+
                 else:
-                    VALS.append([X_TOTAL/(2*NUM_LINES), Y_TOTAL/(2*NUM_LINES)])
-    
-            for LINE in LINES:
-                x1, y1, x2, y2=LINE[0]
-                cv2.line(LINE_IMG, (x1, y1), (x2, y2), (0, 255, 0), 1)
+                    VALS.append([X_TOTAL / (2 * NUM_LINES), Y_TOTAL / (2 * NUM_LINES)])
+
+            if debugging:
+                for LINE in LINES:
+                    x1, y1, x2, y2 = LINE[0]
+                    cv2.line(LINE_IMG, (x1, y1), (x2, y2), (0, 255, 0), 1)
         else:
             sd.putBoolean("Sees Target", False)
     else:
         sd.putBoolean("Sees Target", False)
 
     # Open the gallery of all my filtered works
-    # cv2.imshow("og lines", LINE_IMG)
-    # cv2.imshow("lines", FILTERED_LINE_IMG)
-    # cv2.imshow('OG', IMG)
-    # cv2.imshow('Mask', MASK)
-    # cv2.imshow('blur', BLUR_EDGES)
-    # cv2.imshow('median', MEDIAN)
-    # cv2.imshow('med', MED_EDGES)
-    # cv2.imshow('Mask Edges', MASK_EDGES)
+    if debugging:
+        cv2.imshow("og lines", LINE_IMG)
+        cv2.imshow("lines", FILTERED_LINE_IMG)
+        cv2.imshow("OG", IMG)
+        cv2.imshow("Mask", MASK)
+        cv2.imshow("blur", BLUR_EDGES)
+        cv2.imshow("median", MEDIAN)
+        cv2.imshow("med", MED_EDGES)
+        cv2.imshow("Mask Edges", MASK_EDGES)
 
     OldStream = isShooting
 
     outputStream.putFrame(IMG)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
+    if cv2.waitKey(1) & 0xFF == ord("q"):
         break
-    end_time=time.time()
-    sd.putNumber("FPS", 1/(end_time - start_time))
+    end_time = time.time()
+    sd.putNumber("FPS", 1 / (end_time - start_time))
 
 cv2.destroyAllWindows()
 pipe.stop()
