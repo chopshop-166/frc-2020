1bc23c751a829b677aa6fe6827dc556675051819
Andrew M <amlefty5@gmail.com>
2021-03-31 19:05:58 -0400

    Robot doesn't error out due to Nan

--- src/main/java/frc/robot/utils/PathPlannerUtils.java
+++ src/main/java/frc/robot/utils/PathPlannerUtils.java
@@ -27,7 +27,7 @@ public class PathPlannerUtils {
         // Fill in pose information
         double x = 0, y = 0, rotation = 0;
         x = lineSplit[3];
-        y = -lineSplit[4];
+        y = 4.572 - lineSplit[4];
         rotation = -Math.toRadians(lineSplit[5]);
 
         Pose2d pose = new Pose2d(x, y, new Rotation2d(rotation));
